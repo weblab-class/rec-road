fd4686e996898ba0e183f167526e582e7d3378e5
Bill Li <wfli@mit.edu>
2022-01-13 20:32:28 -0500

    set up mong connection URL and database name

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://wfli2:mcdonalds@cluster0.jzjks.mongodb.net/mickiesDB?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "mickiesDB";
 
 // connect to mongodb
 mongoose
