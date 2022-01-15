49457deee479d142610b3fc05f83f38951b009ee
Bill Li <wfli@mit.edu>
2022-01-15 04:25:38 -0500

    continue working on feed

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -9,10 +9,9 @@ const Feed = (props) => {
   // called when the "Feed" component "mounts", i.e.
   // when it shows up on screen
   useEffect(() => {
-    document.title = "News Feed";
+    document.title = "Course Feed";
     get("/api/stories").then((storyObjs) => {
-      let reversedStoryObjs = storyObjs.reverse();
-      setStories(reversedStoryObjs);
+      setStories(storyObjs);
     });
   }, []);
 
@@ -20,16 +19,17 @@ const Feed = (props) => {
   let storiesList = null;
   const hasStories = stories.length !== 0;
   if (hasStories) {
-    storiesList = stories.map((storyObj) => (
-      <Card
-        key={`Card_${storyObj._id}`}
-        _id={storyObj._id}
-        creator_name={storyObj.creator_name}
-        creator_id={storyObj.creator_id}
-        userId={props.userId}
-        content={storyObj.content}
-      />
-    ));
+    storiesList = stories
+    // storiesList = stories.map((storyObj) => (
+    //   <Card
+    //     key={`Card_${storyObj._id}`}
+    //     _id={storyObj._id}
+    //     creator_name={storyObj.creator_name}
+    //     creator_id={storyObj.creator_id}
+    //     userId={props.userId}
+    //     content={storyObj.content}
+    //   />
+    // ));
   } else {
     storiesList = <div>No stories!</div>;
   }
