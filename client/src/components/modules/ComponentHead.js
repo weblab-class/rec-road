48f6c20e96651ede7a1299be7c854866508c3caa
Bill Li <wfli@mit.edu>
2022-01-15 03:59:46 -0500

    Component header added

---
+++ client/src/components/modules/ComponentHead.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./ComponentHead.css"
+import "../../utilities.css"
+
+/**
+ * ComponentHead is a component that serves as a header for other components.
+ *
+ * Proptypes
+ * @param {string} content of the component head
+ */
+
+const ComponentHead = (props) => {
+    return (
+        <div className="ComponentHead-container u-bold">
+            props.content
+        </div>
+    )
+}
