f8e56dde2b3ce184d2fea534b504839bd702e3b1
Helena Liu <helenaliu@dhcp-10-31-83-56.dyn.mit.edu>
2022-01-15 22:52:59 -0500

    changed font

--- client/src/components/modules/CentralFeed.js
+++ client/src/components/modules/CentralFeed.js
@@ -4,49 +4,70 @@ import Card from "../modules/Card.js";
 import { get } from "../../utilities";
 import useCourseSearch from "./useCourseSearch.js";
 
+const CentralFeed = () => {
+  //const [stories, setStories] = useState([]);
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
 
-const CentralFeed = () =>{
-    // const [stories, setStories] = useState([])
-    const [query, setQuery] = useState('')
-    const [pageNumber, setPageNumber] = useState(1)
+  /*useEffect(() => {
+    get("/api/stories").then((storyObjs) => {
+      setStories(storyObjs);
+    });
+  }, []);
 
-    const {courses, hasMore, loading, error} = useCourseSearch(query, pageNumber)
+  useEffect(() => {
+    console.log(stories);
+  }, [stories]);*/
 
-    const observer = useRef()
-    
-    const lastCourseElementRef = useCallback( node => {
-      if (loading) return
-      if (observer.current) observer.current.disconnect()
-      observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting && hasMore){
-          setPageNumber(prevPageNumber => prevPageNumber + 1)
+  const { courses, hasMore, loading, error } = useCourseSearch(query, pageNumber);
+
+  const observer = useRef();
+
+  const lastCourseElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
         }
-      })
-      if (node) observer.current.observe(node)
-    }, [loading, hasMore])
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
-    const handleSearch = (e) => {
-      setQuery(e.target.value)
-      setPageNumber(1)
-    }
-    
-    // useEffect(() => {
-    //     get("/api/stories").then((storyObjs) => {
-    //       setStories(storyObjs);
-    //     });
-    //   }, []);
-    return <div>
-        <input type="text" value={query} onChange={handleSearch}></input>
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
+  // useEffect(() => {
+  //     get("/api/stories").then((storyObjs) => {
+  //       setStories(storyObjs);
+  //     });
+  //   }, []);
+  return (
+    <div class="u-textCenter">
+      <input type="text" value={query} onChange={handleSearch}></input>
+      <link rel="stylesheet" href="../../utilities.css" />
+      <div>
         {courses.map((course, index) => {
-          if (course.length === index + 1){
-            return <div ref={lastCourseElementRef} key={course}>{course}</div>
+          if (course.length === index + 1) {
+            return (
+              <div ref={lastCourseElementRef} key={course}>
+                {course}
+              </div>
+            );
           } else {
-            return <div key={course}>{course}</div>
+            return <div key={course}>{course}</div>;
           }
         })}
-        <div>{loading && 'Loading...'}</div>
-        <div>{error && 'Error...'}</div> 
+      </div>
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error..."}</div>
     </div>
-}
+  );
+};
 
-export default CentralFeed
+export default CentralFeed;
