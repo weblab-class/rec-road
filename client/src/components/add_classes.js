45fc96014798572af2ca6b1f3ef56d878d031520
Bill Li <wfli@mit.edu>
2022-01-16 17:26:20 -0500

    deleted full.js and added better google login

--- client/src/components/add_classes.js
+++ client/src/components/add_classes.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import { post } from "../utilities";
-import {classData} from "./full"
+//import {classData} from "./full"
 import axios from  'axios'
 
 
