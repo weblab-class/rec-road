93ac0e50c40205848fa4c8094addd3a434c1fd0a
Bill Li <wfli@mit.edu>
2022-01-20 22:26:25 -0500

    redirect login

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,5 +1,9 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
+import {navigate} from "@reach/router";
+// import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+
 
 import "../../utilities.css";
 import "./Skeleton.css";
@@ -7,24 +11,32 @@ import "./Skeleton.css";
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "876948856050-m02nj91vv8in97n334u1ldug2010avfa.apps.googleusercontent.com";
 
-const Skeleton = ({ userId, handleLogin, handleLogout }) => {
+
+const Login = (props) => {
+  
+  useEffect(()=>{
+    if (props.userId){
+      navigate("/")
+  }
+  }, [props.userId])
+  
   return (
     <>
       <div className="u-textCenter">
         <h2>Please login first:</h2>
 
-        {userId ? (
+        {props.userId ? (
           <GoogleLogout
             clientId={GOOGLE_CLIENT_ID}
             buttonText="Logout"
-            onLogoutSuccess={handleLogout}
+            onLogoutSuccess={props.handleLogout}
             onFailure={(err) => console.log(err)}
           />
         ) : (
           <GoogleLogin
             clientId={GOOGLE_CLIENT_ID}
             buttonText="Login"
-            onSuccess={handleLogin}
+            onSuccess={props.handleLogin}
             onFailure={(err) => console.log(err)}
           />
         )}
@@ -33,4 +45,4 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   );
 };
 
-export default Skeleton;
+export default Login;
