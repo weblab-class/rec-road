f7086f33c25bf0d75925109de1fee18b9ae3515e
Bill Li <wfli@mit.edu>
2022-01-25 03:40:48 -0500

    removed firehose code and first edition scraper code

--- firehose-master/new_scripts/update_schedule.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-cd "${0%/*}"
-echo "=== coursews.py ==="
-python3 coursews.py
-echo "=== sublist_ws.py ==="
-python3 sublist_ws.py
-echo "=== combiner_ws.py ==="
-python3 combiner_ws.py
-cp full.json ../www/full.js
